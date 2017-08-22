6edb17409fc3fe459d4efc11d77147e0a2dfe0a3
Piotr Sekula <Piotr_Sekula@epam.com>
2017-08-22 13:23:46 +0200

    Add tests for auditorium service

--- src/test/java/ua/epam/spring/hometask/domain/impl/AuditoriumServiceImplTest.java
+++ src/test/java/ua/epam/spring/hometask/domain/impl/AuditoriumServiceImplTest.java
@@ -8,13 +8,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ua.epam.spring.hometask.domain.Auditorium;
 import ua.epam.spring.hometask.service.AuditoriumService;
 
+import java.util.Set;
+
 public class AuditoriumServiceImplTest {
 
     private ConfigurableApplicationContext context;
 
     @Before
     public void before() {
-        context = new ClassPathXmlApplicationContext("audit.xml");
+        context = new ClassPathXmlApplicationContext("auditoriums.xml");
     }
 
     @Test
@@ -27,5 +29,17 @@ public class AuditoriumServiceImplTest {
 
         // Then
         Assert.assertEquals(50, getAuditorium.getNumberOfSeats());
+    }
+
+    @Test
+    public void shouldGetAllAuditoriums() {
+        // Given
+        AuditoriumService service = context.getBean("auditoriumService", AuditoriumService.class);
+
+        // When
+        Set<Auditorium> getCollection = service.getAll();
+
+        // Then
+        Assert.assertEquals(4, getCollection.size());
     }
 }
