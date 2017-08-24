c86af6d57e86435f5175757298c7e54852d18b1c
Piotr Sekula <piotr92345@o2.pl>
2017-08-24 11:21:51 +0200

    Add test for auditorium

--- src/main/java/ua/epam/spring/hometask/impl/AuditoriumServiceImpl.java
+++ src/main/java/ua/epam/spring/hometask/impl/AuditoriumServiceImpl.java
@@ -1,19 +1,72 @@
 package ua.epam.spring.hometask.impl;
 
+import org.springframework.stereotype.Service;
 import ua.epam.spring.hometask.domain.Auditorium;
 import ua.epam.spring.hometask.service.AuditoriumService;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+@Service
 public class AuditoriumServiceImpl implements AuditoriumService {
 
-    private Set<Auditorium> auditoriums;
+    private Set<Auditorium> auditoriums = new HashSet<>();
 
-    public AuditoriumServiceImpl(Set<Auditorium> auditoriums) {
-        this.auditoriums = auditoriums;
+    public AuditoriumServiceImpl(Set<String> propertiesFiles) {
+        auditoriums = propertiesFiles.stream().map(fileName -> {
+            try {
+                InputStream inputStream = new FileInputStream(fileName);
+                Properties properties = new Properties();
+                properties.load(inputStream);
+
+                Auditorium auditorium = new Auditorium();
+                auditorium.setName(properties.getProperty("name"));
+                auditorium.setNumberOfSeats(Long.valueOf(properties.getProperty("numberOfSeats")));
+
+                Set<Long> vipSeats = Arrays.stream(properties.getProperty("vipSeats").split(","))
+                        .map(Long::valueOf)
+                        .collect(Collectors.toSet());
+                auditorium.setVipSeats(vipSeats);
+
+                return auditorium;
+            } catch (IOException e) {
+                throw new IllegalArgumentException("Property not found: " + e.getMessage() + "; " + e.getCause());
+            }
+        }).collect(Collectors.toSet());
     }
+
+/*try {
+
+        input = new FileInputStream("config.properties");
+
+        // load a properties file
+        prop.load(input);
+
+        // get the property value and print it out
+        System.out.println(prop.getProperty("database"));
+        System.out.println(prop.getProperty("dbuser"));
+        System.out.println(prop.getProperty("dbpassword"));
+
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    } finally {
+        if (input != null) {
+            try {
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }*/
 
     @Nonnull
     @Override
--- src/test/java/ua/epam/spring/hometask/domain/impl/AuditoriumServiceImplTest.java
+++ src/test/java/ua/epam/spring/hometask/domain/impl/AuditoriumServiceImplTest.java
@@ -1,45 +1,63 @@
 package ua.epam.spring.hometask.domain.impl;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import ua.epam.spring.hometask.domain.Auditorium;
+import ua.epam.spring.hometask.impl.AuditoriumServiceImpl;
 import ua.epam.spring.hometask.service.AuditoriumService;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
 public class AuditoriumServiceImplTest {
 
-    private ConfigurableApplicationContext context;
+    @Configuration
+    static class ContextConfiguration {
 
-    @Before
-    public void before() {
-        context = new ClassPathXmlApplicationContext("auditoriums.xml");
+        @Bean
+        AuditoriumService auditoriumService() {
+            Set<String> propertyFiles = new HashSet<>();
+            propertyFiles.add("src/main/resources/big.properties");
+            propertyFiles.add("src/main/resources/medium.properties");
+            propertyFiles.add("src/main/resources/small.properties");
+            propertyFiles.add("src/main/resources/exclusive.properties");
+
+            AuditoriumService auditoriumService = new AuditoriumServiceImpl(propertyFiles);
+            return auditoriumService;
+        }
     }
+
+    @Autowired
+    private AuditoriumService service;
 
     @Test
     public void shouldGetAuditoriumByName() {
-        // Given
-        AuditoriumService service = context.getBean("auditoriumService", AuditoriumService.class);
-
         // When
-        Auditorium getAuditorium = service.getByName("Small");
+        Auditorium getAuditorium = service.getByName("Big");
 
         // Then
-        Assert.assertEquals(50, getAuditorium.getNumberOfSeats());
+        assertEquals(100, getAuditorium.getNumberOfSeats());
+        assertTrue(getAuditorium.getVipSeats().containsAll(Arrays.asList(82L, 83L, 84L, 85L, 86L, 87L)));
     }
 
     @Test
     public void shouldGetAllAuditoriums() {
-        // Given
-        AuditoriumService service = context.getBean("auditoriumService", AuditoriumService.class);
-
         // When
         Set<Auditorium> getCollection = service.getAll();
 
         // Then
-        Assert.assertEquals(4, getCollection.size());
+        assertEquals(4, getCollection.size());
     }
 }
